b36087b4d7a5a031d916024a61c91c74c39ff4d4
DMPMS <monteiro_davi@yahoo.com>
2020-06-27 17:27:00 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/monteirodavi/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/monteirodavi/cursomc/resources/CategoriaResource.java
@@ -66,13 +66,12 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
-			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
-		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));  
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 
 	}
